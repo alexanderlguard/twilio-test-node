4bdc7091fe3391dbd7b06ed0a2c00a74d01d2cc1
Alexander Guardado <72055339+alexanderlguard@users.noreply.github.com>
2021-07-19 17:08:31 -0700

    second

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-const http = require('http');
-const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.post('/sms', (req, res) => {
-    const twiml = new MessagingResponse();
+app.listen(port, () => {
+  console.log(`Test Port: ${port}`)
+})
+
+
+// const http = require('http');
+// const express = require('express');
+// const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+// const app = express();
+
+// app.post('/sms', (req, res) => {
+//     const twiml = new MessagingResponse();
   
-    twiml.message('The Robots are coming! Head for the hills!');
+//     twiml.message('The Robots are coming! Head for the hills!');
   
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-  });
+//     res.writeHead(200, {'Content-Type': 'text/xml'});
+//     res.end(twiml.toString());
+//   });
   
-  http.createServer(app).listen(1337, () => {
-    console.log('Express server listening on port 1337');
-  });
+//   http.createServer(app).listen(1337, () => {
+//     console.log('Express server listening on port 1337');
+//   });
 
-const accountSid = 'AC5ae6684bd7945e1ad2515bde21437117';
-const authToken = '1b80f25529a2b51f03fff3d5b60005fe';
-const client = require('twilio')(accountSid, authToken);
+// const accountSid = 'AC5ae6684bd7945e1ad2515bde21437117';
+// const authToken = '1b80f25529a2b51f03fff3d5b60005fe';
+// const client = require('twilio')(accountSid, authToken);
 
-// client.messages
-//   .create({
-//      body: 'This is a message for test',
-//      from: '+18324301022',
-//      to: '+18312247870'
-//    })
-//   .then(message => console.log(message.sid));
+// // client.messages
+// //   .create({
+// //      body: 'This is a message for test',
+// //      from: '+18324301022',
+// //      to: '+18312247870'
+// //    })
+// //   .then(message => console.log(message.sid));
