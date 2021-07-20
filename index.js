58fe328409b58b0282ef223497cf0c675d01025c
Alexander Guardado <72055339+alexanderlguard@users.noreply.github.com>
2021-07-19 17:42:46 -0700

    work!

--- index.js
+++ index.js
@@ -1,58 +1,39 @@
-const express = require('express')
-const app = express()
-
+// -----------------------------------------
 const accountSid = 'AC5ae6684bd7945e1ad2515bde21437117';
 const authToken = '1b80f25529a2b51f03fff3d5b60005fe';
 
+const express = require('express');
+const bodyParser = require('bodyParser');
+const twilio = require('twilio');
+
+const client = new twilio(accountSid, authToken);
+const app = express();
+
 const port = process.env.PORT || 3000;
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const MessagingResponse = twilio.twiml.MessagingResponse;
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+    res.send('This is just a TEST!')
+});
 
 app.post('/sms', (req, res) => {
+    let from = req.body.from;
+    let to = req.body.to;
+
     const twiml = new MessagingResponse();
   
-    twiml.message('The Robots are coming! Head for the hills!');
+    twiml.message(`From: ${from} To: ${to}`);
   
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
 });
 
+
 app.listen(port, () => {
-  console.log(`Test Port: ${port}`)
-})
+    console.log(`The server is working on the port ${port}`)
+});
+// -----------------------------------------
 
-
-// const http = require('http');
-// const express = require('express');
-// const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
-// const app = express();
-
-// app.post('/sms', (req, res) => {
-//     const twiml = new MessagingResponse();
-  
-//     twiml.message('The Robots are coming! Head for the hills!');
-  
-//     res.writeHead(200, {'Content-Type': 'text/xml'});
-//     res.end(twiml.toString());
-//   });
-  
-//   http.createServer(app).listen(1337, () => {
-//     console.log('Express server listening on port 1337');
-//   });
-
-// const accountSid = 'AC5ae6684bd7945e1ad2515bde21437117';
-// const authToken = '1b80f25529a2b51f03fff3d5b60005fe';
-// const client = require('twilio')(accountSid, authToken);
-
-// // client.messages
-// //   .create({
-// //      body: 'This is a message for test',
-// //      from: '+18324301022',
-// //      to: '+18312247870'
-// //    })
-// //   .then(message => console.log(message.sid));
